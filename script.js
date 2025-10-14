c38110326bb0a1dbfec0b0bfe65b5e34af2d6c23
adrianfadhilw <adrianfadhilw@gmail.com>
2025-10-14 11:47:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa=document.getElemenById('sapaButton')
+const tombolSapa=document.getElementById('sapaButton')
 tombolSapa.addEventListener('click',function(){
 alert('Hello! Thank you for coming!')
 });
